b5041d037e03795989717c2b284c81a53bb8e545
NetMet1 <36077940+netmet1@users.noreply.github.com>
2022-08-03 08:36:11 -0400

    migrate mainnet and testnet title to overview update wording

--- sidebarsNodes.js
+++ sidebarsNodes.js
@@ -6,22 +6,8 @@ module.exports = {
       collapsed: false,
       items: [
         'index', 
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Mainnet Nodes',
-      collapsed: false,
-      items: [
-        'mainnet/intro',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Testnet Nodes',
-      collapsed: false,
-      items: [
-        'testnet/intro',
+        'mainnet-overview',
+        'testnet-overview'
       ],
     },
     {
