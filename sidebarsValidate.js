f2fe12fc23fe21147151ce97d0e452b4a4a53caf
NetMet1 <36077940+netmet1@users.noreply.github.com>
2024-04-23 16:46:40 -0400

    update to upgrade procedure

--- sidebarsValidate.js
+++ sidebarsValidate.js
@@ -136,6 +136,8 @@ module.exports = {
               items: [
                 'automated/upgrade/nodectlUpgradeIntro',
                 'automated/upgrade/nodectlUpgradeStart',
+                'automated/upgrade/nodectlUpgradeMigrate',
+                'automated/upgrade/nodectlUpgradeStart2',
                 'automated/upgrade/nodectlUpgradeConfirm',
                 'automated/upgrade/nodectlUpgradeSetup',
                 'automated/upgrade/nodectlUpgradeVersion',
